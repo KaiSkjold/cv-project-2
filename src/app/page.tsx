99373668d5a7db394a93710079e6fb9df91d33e4
KaiSkjold <skjoldann@gmail.com>
2025-03-13 14:33:38 +0100

    added navigation with react router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,41 @@
+"use client";
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { CoverLetterPage } from './components/CoverLetterPage';
 import { ResumePage } from './components/ResumePage';
+import { WelcomePage } from './welcomePage';
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // TODO should I add a loading spinner here?
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-2/5">
-      <nav className="w-full bg-blue-950 text-amber-400 p-4 fixed top-0 left-0 right-0">
-        <ul className='flex gap-4'>
-          <li>test 1</li>
-          <li>test 2</li>
-          <li>test 3</li>
-        </ul>
-      </nav>
-      <main >
-    
-        <CoverLetterPage />
-        <ResumePage />
-
-
-
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      </footer>
-    </div>
+    <Router>
+      <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-2/5">
+        <nav className="w-full bg-blue-950 text-amber-400 p-4 fixed top-0 left-0 right-0">
+          <ul className='flex gap-4'>
+            <li><Link to="/">Welcome Page</Link></li>
+            <li><Link to="/cover-letter">Cover Letter</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+          </ul>
+        </nav>
+        <main>
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/cover-letter" element={<CoverLetterPage />} />
+            <Route path="/resume" element={<ResumePage />} />
+          </Routes>
+        </main>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        </footer>
+      </div>
+    </Router>
   );
 }
---
+++ src/app/welcomePage.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const WelcomePage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl text-amber-400 bg-blue-950 p-4 border-amber-400 rounded-t-lg">Welcome</h1>
+            <h2 className=" bg-blue-950 text-amber-400 text-xl p-3">Here we will help you make the best possible application for your dream job</h2>
+            <img src="https://spaceholder.cc/i/1000x1000" alt="Space because it's all there is"/>
+            <div className="bg-blue-950 rounded-b-lg text-amber-400 p-3">Let`s get started!</div>
+        </div>
+    );
+};
