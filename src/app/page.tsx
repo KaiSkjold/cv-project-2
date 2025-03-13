ecb219ddc9842c7ebdea82f4cdf1062ac75b4769
KaiSkjold <skjoldann@gmail.com>
2025-03-13 13:08:49 +0100

    added navbar styling, no functionality yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import { ResumePage } from './components/ResumePage';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-2/5">
+      <nav className="w-full bg-blue-950 text-amber-400 p-4 fixed top-0 left-0 right-0">
+        <ul className='flex gap-4'>
+          <li>test 1</li>
+          <li>test 2</li>
+          <li>test 3</li>
+        </ul>
+      </nav>
+      <main >
     
-      <CoverLetterPage />
-      <ResumePage />
+        <CoverLetterPage />
+        <ResumePage />
 
 
 
